a6d6e24d2197d77f1de5a9038687dc4e9ef06f43
Galsmont <44008111+Galsmont@users.noreply.github.com>
2018-10-19 20:46:40 -0300

    Add files via upload

---
+++ Aula08/07_Metodo/src/controller/ChamadaMetodos.java
@@ -0,0 +1,25 @@
+package controller;
+
+public class ChamadaMetodos {
+
+	public static void main (String[] args) {
+		System.out.println("Iniciando Programa");
+		primeiro(); 
+		segundo();
+		terceiro();
+	}
+
+public static void terceiro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void primeiro() {
+		// TODO Auto-generated method stub
+	}	
+		public static void segundo() {
+		// TODO Auto-generated method stub
+		
+	}			
+		
+}
