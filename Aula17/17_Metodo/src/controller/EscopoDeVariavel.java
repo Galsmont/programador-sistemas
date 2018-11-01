54d5e4ffcf982dd07dbffa68b3bc10a1ac2653bc
Galsmont <44008111+Galsmont@users.noreply.github.com>
2018-10-31 21:38:47 -0300

    Add files via upload

---
+++ Aula17/17_Metodo/src/controller/EscopoDeVariavel.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class EscopoDeVariavel {
+	
+	public static void main (String [] args) {
+		metodoA ();
+		metodoB ();
+		int x =5;
+		System.out.println("x no inicio de main () é " + x);
+	}
+	static int x = 1; //variável global
+	public static void metodoA() {
+		int x = 25;
+		System.out.println("Variavel local x ao entrar no metodoA é" + x);
+		x = x + 1;
+		System.out.println("Variavel local x antes de sair do metodoA é" + x);
+	}
+		public static void metodoB() {
+			System.out.println ("Variavel x ao entrar no metodoB é" + x + " global");
+			x = x*10;
+			System.out.println("Variavel x antes de sair do metodoB é" + x);
+	}
+}
+
