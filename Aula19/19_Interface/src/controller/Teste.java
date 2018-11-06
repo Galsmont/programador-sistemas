2776f09748022f8206d053d4a91cc0eb559f895b
Galsmont <44008111+Galsmont@users.noreply.github.com>
2018-11-06 21:37:27 -0200

    Add files via upload

---
+++ Aula19/19_Interface/src/controller/Teste.java
@@ -0,0 +1,17 @@
+package controller;
+import model .*;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Usuário usu = new Usuário ();
+		Professor prof = new Professor ("joaquim", "joaquim123", 0, "joaquim da silva", "Doutor", 1123);
+			prof.mostrar();
+			System.out.println("------------------");
+		
+		Aluno aluno = new Aluno ("muzi", "muzi123", 1, "Sergio Muzi", "primeiro", "tarde", 32115);
+		aluno.mostrar();
+	}
+
+}
